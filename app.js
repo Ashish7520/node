78cce52ec560a84edb9e34e4939d2ba869c0328d
Ashish7520 <120584144+Ashish7520@users.noreply.github.com>
2023-01-14 12:17:47 +0530

    routes, body-parser

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+//importing express
 const express = require('express');
+
+//importing bodyparser
+
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+app.use( "/add-product" , (req, res, next) => {
+    console.log('In another middleware!');
+    res.send("<form action='/product' method='POST'><label>name of product</label><br><input type='text' name='title'><br><label>size</label><br><input type='number' name='size'><br><button type='submit'>Add Product</button></form>");
 });
 
-app.use((req, res, next) => {
+app.use( "/product" , (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+});
+
+app.use( "/" , (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
